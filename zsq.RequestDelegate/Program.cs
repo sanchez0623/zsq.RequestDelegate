b3cecedaff34c3c9518e4475d9682d32bdc48899
sanchez0623 <18576645357@163.com>
2018-12-15 16:32:40 +0800

    init

---
+++ zsq.RequestDelegate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zsq.RequestDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
